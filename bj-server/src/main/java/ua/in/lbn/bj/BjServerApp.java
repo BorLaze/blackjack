94cc076a4dbda902eaa4cbfd1734ed826e9a66bb
BorLaze <borlaze@gmail.com>
2020-10-16 16:23:16 +0300

    add server spring-boot stub

---
+++ bj-server/src/main/java/ua/in/lbn/bj/BjServerApp.java
@@ -0,0 +1,12 @@
+package ua.in.lbn.bj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjServerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjServerApp.class, args);
+    }
+}
