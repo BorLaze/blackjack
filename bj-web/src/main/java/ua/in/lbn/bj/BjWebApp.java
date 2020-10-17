80c50c22de538d17afb338d7d3debf38e1a8a48a
BorLaze <borlaze@gmail.com>
2020-10-17 11:13:43 +0300

    add web stub

---
+++ bj-web/src/main/java/ua/in/lbn/bj/BjWebApp.java
@@ -0,0 +1,13 @@
+package ua.in.lbn.bj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BjWebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BjWebApp.class, args);
+    }
+
+}
